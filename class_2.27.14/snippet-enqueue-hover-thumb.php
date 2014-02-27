128d8137b7a590edbfb9a4d1e956bdd2ba226055
Christopher Stein <prof.stein@gmail.com>
2014-02-27 17:14:19 -0500

    Files for 2.27.14
    These files are mostly to show how to add in thumbnails that show info
    on over for the homepage. Today we also worked on Base Foundation folder

---
+++ class_2.27.14/snippet-enqueue-hover-thumb.php
@@ -0,0 +1,8 @@
+ <?php  
+
+
+  //CSS for the Front-page hover thumbnails
+  wp_enqueue_style( 'hover-thumb', get_template_directory_uri() . '/css/hover-thumb.css', array('portfolio-style', 'normalize') );
+
+
+?>
